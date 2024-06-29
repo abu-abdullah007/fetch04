06834a610ab4ec6c9b0b9d7a896c4443a514dffb
abu-abdullah007 <zabernakib12580@gmail.com>
2024-06-29 13:15:40 -0400

    api json created

---
+++ main.js
@@ -0,0 +1,29 @@
+const card = document.querySelector('.card')
+const myName = card.querySelector('#myName')
+const email = card.querySelector('#email')
+const password = card.querySelector('#password')
+const img_one = card.querySelector('#one')
+const img_two = card.querySelector('#two')
+const img_three = card.querySelector('#three')
+const img_four = card.querySelector('#four')
+const button = card.querySelector('#mainBtn')
+
+async function getDataFromServer(){
+    const server_response = await fetch('http://127.0.0.1:5500/randomProjects/fetch04/api.json')
+    const receved_data = await server_response.json()
+
+    myName.innerHTML = receved_data.name
+    email.innerHTML = receved_data.email
+    password.innerHTML = receved_data.password
+    img_one.src = receved_data.image1
+    img_two.src = receved_data.image2
+    img_three.src = receved_data.image3
+    img_four.src = receved_data.image4
+
+    button.innerHTML = "Data Loaded";
+    setTimeout(()=>{
+        button.innerHTML = "Click To Get Data"
+    },2000)
+}
+
+button.addEventListener("click",getDataFromServer)
